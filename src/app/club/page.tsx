81de4db499b726ed89ef1526892e489748304329
Ruba <rubarubs575@gmail.com>
2025-06-26 13:27:59 +0530

    Updated club page

--- src/app/club/page.tsx
+++ src/app/club/page.tsx
@@ -1,19 +1,5 @@
-//replace with club buttons and stuff accroding to design
-import { BusButtons } from "@/components/Bus/busButton";
-
-const busData = [
-  { slug: 'bus-1', name: 'Bus 1' },
-  { slug: 'bus-2', name: 'Bus 2' },
-  { slug: 'bus-3', name: 'Bus 3' },
-  { slug: 'bus-4', name: 'Bus 4' },
-  { slug: 'bus-5', name: 'Bus 5' },
-  { slug: 'bus-6', name: 'Bus 6' },
-];
-
-export default function BusPage() {
-  return (
-    <div>
-      <BusButtons buses={busData} />
-    </div>
-  );
+'use client';
+import ClubList from "@/components/club/ClubList";
+export default function ClubPage() {
+  return <ClubList/>;
 }
