6be934a94058b4f86a75659edd599f9837134ddc
shadil-rayyan <shadilrayyan2@gmail.com>
2025-06-29 22:59:17 +0530

    fix(build):removed some code in middleware and api auth cookie to fix npm run build

--- src/app/api/set-auth-cookie/route.ts
+++ src/app/api/set-auth-cookie/route.ts
@@ -1,19 +1,19 @@
 import { NextRequest, NextResponse } from 'next/server';
 // import { getAuth } from 'firebase-admin/auth';
-import { initializeApp, getApps, cert } from 'firebase-admin/app';
+// import { initializeApp, getApps, cert } from 'firebase-admin/app';
 import { serialize } from 'cookie';
 
-const serviceAccount = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-};
+// const serviceAccount = {
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+//   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+// };
 
-if (!getApps().length) {
-  initializeApp({
-    credential: cert(serviceAccount as any),
-  });
-}
+// if (!getApps().length) {
+//   initializeApp({
+//     credential: cert(serviceAccount as any),
+//   });
+// }
 
 export async function POST(req: NextRequest) {
   try {
