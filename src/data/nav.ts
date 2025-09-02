fcda7b6ec8c004ed8903107a806582726a551b88
shadil-rayyan <shadilrayyan2@gmail.com>
2025-09-02 17:36:27 +0530

    fix(studymaterial):redirected to ktu notes for now

--- src/data/nav.ts
+++ src/data/nav.ts
@@ -2,7 +2,7 @@ import { Book, Calendar2, Calendar, Home2, Money } from "iconsax-react";
 
 export const bottomTabs = [
   { name: "home", icon: Home2, href: "/", label: "Home" },
-  { name: "studymaterial", icon: Book, href: "/studymaterial", label: "Study" },
+  { name: "studymaterial", icon: Book, href: "/https://www.ktunotes.in/", label: "Study" },
   { name: "attendance", icon: Calendar2, href: "/attendance/calendar", label: "Attendance" },
   { name: "finance", icon: Money, href: "/expense", label: "Finance" },
   { name: "event", icon: Calendar, href: "/events", label: "Event" },
