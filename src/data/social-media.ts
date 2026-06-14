db447cfa299754f6a254f52ec966b793e0e42bf7
SHADIL AM <109820522+shadil-rayyan@users.noreply.github.com>
2026-06-14 22:54:39 +0530

    Create social-media.ts

---
+++ src/data/social-media.ts
@@ -0,0 +1,128 @@
+export interface SocialLink {
+  name: string;
+  platform: "linkedin" | "reddit" | "instagram";
+  url: string;
+}
+
+export const socialLinks: SocialLink[] = [
+  // LinkedIn
+  {
+    name: "CSE GECPKD",
+    platform: "linkedin",
+    url: "https://www.linkedin.com/company/csegecskp/posts/?feedView=all",
+  },
+  {
+    name: "Placement Cell GECPKD",
+    platform: "linkedin",
+    url: "https://www.linkedin.com/company/placementcellgecpkd/posts/?feedView=all",
+  },
+  {
+    name: "GECPKD",
+    platform: "linkedin",
+    url: "https://www.linkedin.com/company/gecskp/",
+  },
+  {
+    name: "CodeChef GECPKD Chapter",
+    platform: "linkedin",
+    url: "https://www.linkedin.com/company/codechef-gecpkd-chapter/posts/",
+  },
+
+  // Reddit
+  {
+    name: "GEC Palakkad Reddit",
+    platform: "reddit",
+    url: "https://www.reddit.com/r/GECPalakkad/s/NGzyAAJhqR",
+  },
+  {
+    name: "Life At Mannampatta",
+    platform: "reddit",
+    url: "https://www.reddit.com/r/LifeAtMannampatta/s/Y73K0MfqGw",
+  },
+
+  // Instagram
+  {
+    name: "Life At GEC",
+    platform: "instagram",
+    url: "https://www.instagram.com/lifeatgec",
+  },
+  {
+    name: "GEC Palakkad",
+    platform: "instagram",
+    url: "https://www.instagram.com/gecpalakkad/",
+  },
+  {
+    name: "IEEE SB GECPKD",
+    platform: "instagram",
+    url: "https://www.instagram.com/ieeesbgecpkd/",
+  },
+  {
+    name: "EEE GECPKD",
+    platform: "instagram",
+    url: "https://www.instagram.com/eee.gecpkd/",
+  },
+  {
+    name: "TinkerHub GECPKD",
+    platform: "instagram",
+    url: "https://www.instagram.com/tinkerhub_gecpkd/",
+  },
+  {
+    name: "CSE GECPKD",
+    platform: "instagram",
+    url: "https://www.instagram.com/cse_gecpkd/",
+  },
+  {
+    name: "ECE GECPKD",
+    platform: "instagram",
+    url: "https://www.instagram.com/ece_gec_pkd/",
+  },
+  {
+    name: "IEDC GECPKD",
+    platform: "instagram",
+    url: "https://www.instagram.com/iedcgecpkd/",
+  },
+  {
+    name: "GDG GEC Palakkad",
+    platform: "instagram",
+    url: "https://www.instagram.com/gdggecpalakkad/",
+  },
+  {
+    name: "IEEE ComSoc GEC Palakkad",
+    platform: "instagram",
+    url: "https://www.instagram.com/ieeecomsocgecpalakkad/",
+  },
+  {
+    name: "She Cell GECP",
+    platform: "instagram",
+    url: "https://www.instagram.com/shecellgecp/",
+  },
+  {
+    name: "ISTE GECPKD",
+    platform: "instagram",
+    url: "https://www.instagram.com/iste.gecpkd/",
+  },
+  {
+    name: "Robotics Club GECPKD",
+    platform: "instagram",
+    url: "https://www.instagram.com/roboticsclubgecpkd/",
+  },
+  {
+    name: "muLearn GECSKP",
+    platform: "instagram",
+    url: "https://www.instagram.com/mulearn.gecskp/",
+  },
+  {
+    name: "SAE GECPKD",
+    platform: "instagram",
+    url: "https://www.instagram.com/sae_gecpkd/",
+  },
+  {
+    name: "Civil GECPKD",
+    platform: "instagram",
+    url: "https://www.instagram.com/civil.gecpkd/",
+  },
+  {
+    name: "Beacon GEC Palakkad",
+    platform: "instagram",
+    url: "https://www.instagram.com/beacon_gec_palakkad/",
+  },
+];
